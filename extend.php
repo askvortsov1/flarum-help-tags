0bd53052dbe1e1a971b2001ab038b4d9f589a267
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-03-01 17:17:37 -0500

    Removed unused files

--- extend.php
+++ extend.php
@@ -19,8 +19,6 @@ return [
     
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
-        
-    new Extend\Locales(__DIR__ . '/resources/locale'),
 
     function (Dispatcher $events) {
         $events->subscribe(Access\DiscussionPolicy::class);
