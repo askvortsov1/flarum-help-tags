5efef8cc804adaa777a4f8abdac18e7bcdfbbce5
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-03-10 00:12:09 -0400

    Hack to ensure that the orWhere is inserted at the right place

--- src/Access/DiscussionPolicy.php
+++ src/Access/DiscussionPolicy.php
@@ -11,7 +11,6 @@ namespace Askvortsov\FlarumHelpTags\Access;
 
 use Flarum\Discussion\Discussion;
 use Flarum\Settings\SettingsRepositoryInterface;
-use Flarum\Tags\Tag;
 use Flarum\User\AbstractPolicy;
 use Flarum\User\User;
 use Illuminate\Database\Eloquent\Builder;
@@ -42,7 +41,32 @@ class DiscussionPolicy extends AbstractPolicy
      */
     public function find(User $actor, Builder $query)
     {
-        // Hide discussions which have tags that the user is not allowed to see.
-        $query->orWhere('user_id', $actor->id);
+        if (! $actor->isGuest()) {
+            // Hack to make sure the "where" clause we're inserting will be the first where statement
+
+            // This will be the 2nd where statement.
+            if ($query->getQuery()->wheres != []) {
+                // It will combine with the following clause  (with no effect, thanks Boolean Algebra!)
+                // and stop it from destabilizing the next clause.
+                array_unshift($query->getQuery()->wheres, [
+                    "type" => "raw",
+                    // Set the sql value to whatever will have no effect on the following clause.
+                    "sql" => $query->getQuery()->wheres[0]['boolean'] === 'and' ? '1': '0',
+                    "boolean" => "or"
+                ]);
+                array_unshift($query->getQuery()->bindings['where'], []);
+            }
+
+            // This will be the 1st where statement.
+            // This always allows the author of a discussion to see the discussion.
+            array_unshift($query->getQuery()->wheres, [
+                "type" => "Basic",
+                "column" => "discussions.user_id",
+                "operator" => "=",
+                "value" => $actor->id,
+                "boolean" => "or"
+            ]);
+            array_unshift($query->getQuery()->bindings['where'], $actor->id);
+        }
     }
 }
