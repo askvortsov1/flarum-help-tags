f78c98b80b85533d70abb1fb0ea88aaca04ece8d
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-25 23:16:49 -0400

    Add JS linting / build config

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,22 +1,27 @@
-import { extend, override } from 'flarum/extend';
-import PermissionGrid from 'flarum/components/PermissionGrid';
+import { extend, override } from "flarum/extend";
+import PermissionGrid from "flarum/components/PermissionGrid";
 
+app.initializers.add("askvortsov/flarum-help-tags", () => {
+  override(app, "getRequiredPermissions", (original, permission) => {
+    var required = original(permission);
 
-app.initializers.add('askvortsov/flarum-help-tags', () => {
-    override(app, 'getRequiredPermissions', (original, permission) => {
-        var required = original(permission);
+    required = required.filter((a) => !/viewDiscussions$/.test(a));
 
-        required = required.filter(a => ! /viewDiscussions$/.test(a))
+    return required;
+  });
 
-        return required;
-    });
-
-    extend(PermissionGrid.prototype, 'viewItems', items => {
-        items.add('viewTag', {
-            icon: 'fas fa-eye',
-            label: app.translator.trans('askvortsov-help-tags.admin.permissions.view_tag_label'),
-            permission: 'discussion.viewTag',
-            allowGuest: true
-        }, 100);
-    });
+  extend(PermissionGrid.prototype, "viewItems", (items) => {
+    items.add(
+      "viewTag",
+      {
+        icon: "fas fa-eye",
+        label: app.translator.trans(
+          "askvortsov-help-tags.admin.permissions.view_tag_label"
+        ),
+        permission: "discussion.viewTag",
+        allowGuest: true,
+      },
+      100
+    );
+  });
 });
