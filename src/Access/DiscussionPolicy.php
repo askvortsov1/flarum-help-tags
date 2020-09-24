bcb7d774ef609431be56723a58a496b5914c13f6
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2020-09-23 23:23:18 -0400

    Mithril 2 update (#6)

--- src/Access/DiscussionPolicy.php
+++ src/Access/DiscussionPolicy.php
@@ -12,7 +12,6 @@
 namespace Askvortsov\FlarumHelpTags\Access;
 
 use Flarum\Discussion\Discussion;
-use Flarum\Settings\SettingsRepositoryInterface;
 use Flarum\User\AbstractPolicy;
 use Flarum\User\User;
 use Illuminate\Database\Eloquent\Builder;
@@ -25,43 +24,13 @@ class DiscussionPolicy extends AbstractPolicy
     protected $model = Discussion::class;
 
     /**
-     * @var SettingsRepositoryInterface
-     */
-    protected $settings;
-
-    /**
-     * @param SettingsRepositoryInterface $settings
-     */
-    public function __construct(SettingsRepositoryInterface $settings)
-    {
-        $this->settings = $settings;
-    }
-
-    /**
      * @param User    $actor
      * @param Builder $query
      */
-    public function find(User $actor, Builder $query)
+    public function findDiscussionsInRestrictedTags(User $actor, Builder $query)
     {
         if (!$actor->isGuest()) {
-            $utilQuery = $query->getQuery()->newQuery();
-            if ($query->getQuery()->wheres[0]['type'] === 'Basic' && $query->getQuery()->wheres[0]['column'] === 'id') {
-                $utilQuery->orWhere(function ($subquery) use ($actor, $query) {
-                    $subquery
-                        ->where('discussions.user_id', $actor->id)
-                        ->where('id', $query->getQuery()->wheres[0]['value']);
-                });
-            } else {
-                $utilQuery->orWhere('discussions.user_id', $actor->id);
-            }
-            if ($query->getQuery()->wheres[0]['boolean'] === 'and') {
-                $utilQuery->orWhereRaw('1');
-            }
-
-            $query->getQuery()->wheres = array_merge($utilQuery->wheres, $query->getQuery()->wheres);
-            $query->getQuery()->bindings['where'] = array_values(
-                array_merge($utilQuery->bindings['where'], $query->getQuery()->bindings['where'])
-            );
+            $query->orWhere('discussions.user_id', $actor->id);
         }
     }
 }
